1804a159bb3bfad0da7f8d95d0651c2602187064
Manisha Singh <manishasinghbca89@gmail.com>
2024-09-24 00:12:51 +0530

    this is the JSX code.

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//This IS JSX code -
+const Heading = <div className="title">header 111</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(Heading);
