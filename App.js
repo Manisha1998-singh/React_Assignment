797e440e1e2dd0161b728a25d1c23871c2bafd01
Manisha Singh <manishasinghbca89@gmail.com>
2024-09-26 06:18:36 +0530

    Functional Component.

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-//This IS JSX code -
-const Heading = () => {
-  return (
-    <div>
-      <div className={title}>header 111</div>;
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-export default Heading;
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    React JSX
+  </h1>
+);
+const List = () => (
+  <div>
+    <ul>
+      <li>Home</li>
+      <li> Contact</li>
+      <li> About</li>
+    </ul>
+  </div>
+);
+const Heading = () => (
+  <div className="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1
+      className="title"
+      style={{ color: "pink", backgroundColor: "red", height: 100 }}>
+      React Functional Component.{" "}
+    </h1>
+    <List></List>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading></Heading>);
