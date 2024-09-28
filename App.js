fb04de0f89b61a4ec2556478cb65252c427ca182
Manisha Singh <manishasinghbca89@gmail.com>
2024-09-28 18:09:38 +0530

    Header using Functional component.

--- App.js
+++ App.js
@@ -2,36 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header.js";
 
-const Title = ({ children }) => (
-  <h1 className="head">{children || "React JSX"}</h1>
-);
-const List = () => (
-  <div>
-    <ul>
-      <li>Home</li>
-      <li> Contact</li>
-      <li> About</li>
-    </ul>
-  </div>
-);
 const Heading = () => (
-  <div className="container">
-    <Header></Header>
-    {/* //{Title()} */}
-    <Title />
-    <Title></Title>
-    {/* 1. Reference to the component, not rendering */}
-    {Title}
-    {/* This is incorrect; it's referencing the function itself  also getting the warnings*/}
-    {/* 2. Immediate rendering without children */}
-    {<Title />}
-    {/* 3. Rendering with children */}
-    {<Title> This is the react functional component</Title>}
-    <h1
-      className="title"
-      style={{ color: "pink", backgroundColor: "red", height: 100 }}>
-      React Functional Component.{" "}
-    </h1>
+  <div className="container-fluid m-0">
+    <Header />
+
     <List></List>
   </div>
 );
---
+++ Components/Header.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <img
+            src="  https://logos-world.net/wp-content/uploads/2020/11/Swiggy-Logo.png"
+            height={20}
+            alt="logo image"
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false">
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit">
+              Search
+            </button>
+          </form>
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/019/879/186/large_2x/user-icon-on-transparent-background-free-png.png"
+            height={30}
+            alt=""
+          />
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Header;
